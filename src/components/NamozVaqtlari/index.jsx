638d5c844a9c2315d6a605f6ac62937554bb7b56
Ulugbek01 <xyz207820@gmail.com>
2022-09-24 14:59:48 +0500

    prayer times section add

--- src/components/NamozVaqtlari/index.jsx
+++ src/components/NamozVaqtlari/index.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../../Layout/Container'
+import { TableWrapper } from './style';
 
 const NamozVaqtlari = () => {
+  const [times, setTimes] = useState([]);
+  const date = new Date();
+
+  useEffect(() => {
+    fetch('https://islomapi.uz/api/monthly?region=Toshkent&month=9')
+      .then((res) => res.json())
+      .then((res) => setTimes(res))
+  }, [])
+
+  console.log(times);
+
   return (
-    <Container>NamozVaqtlari</Container>
+    <Container>
+      <TableWrapper>
+            <table width={'100%'}>
+                <thead className='table-head'>
+                    <tr>
+                        <th>Sentyabr</th>
+                        <th>Hafta kuni</th>
+                        <th>Tong <span>(Saharlik)</span></th>
+                        <th>Quyosh</th>
+                        <th>Pеshin</th>
+                        <th>Asr</th>
+                        <th>Shom <span>(Iftor)</span></th>
+                        <th>Xufton</th>
+                    </tr>
+                </thead>
+                <tbody className='table-body'>
+                    {times.map((item) => 
+                        <tr key={item.day} className={`to-center ${item.weekday.toLowerCase() === 'juma'.toLowerCase() && 'juma'}  ${item.day === date.getDate() && 'active-day'} ${date.getDate() > item.day && 'passive-day'}`}>
+                            <td>{item.day}</td>
+                            <td>{item.weekday}</td>
+                            <td>{item.times?.tong_saharlik}</td>
+                            <td>{item.times?.quyosh}</td>
+                            <td>{item.times?.peshin}</td>
+                            <td>{item.times?.asr}</td>
+                            <td>{item.times?.shom_iftor}</td>
+                            <td>{item.times?.hufton}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </TableWrapper>
+    </Container>
   )
 }
 
-export default NamozVaqtlari
+export default NamozVaqtlari;
--- src/components/NamozVaqtlari/style.js
+++ src/components/NamozVaqtlari/style.js
@@ -1,2 +1,28 @@
 import styled from "styled-components";
 
+export const TableWrapper = styled.div`
+
+    .table-body td{
+        padding: 10px 0;
+        color: #212529;
+    }
+
+    .active-day td{
+        background-color: #1CC465;
+        color: #fff;
+    }
+
+    .passive-day td{
+        color: rgba(0, 0, 0, 0.4);
+    }
+
+    .passive-day.juma td {
+        color: #85C3A1;
+    }
+
+    .juma td{
+        color: #0C8744;
+        font-weight: bold;
+    }
+
+`;
